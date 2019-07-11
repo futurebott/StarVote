81ab3eb59efdc018cedcb3abd090674e88a11663
devaddict1915 <devaddict@outlook.com>
2019-07-11 17:59:17 -0400

    Adding submit form

--- src/Card/Rating.js
+++ src/Card/Rating.js
@@ -2,19 +2,27 @@ import React from 'react';
 class  RatingInput extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { submitRating: 0 }
+        this.state = { rating: 1 }
+        this.ClickHandle = this.ClickHandle.bind(this);
     }
      ClickHandle = (event) => {
         event.preventDefault();
-        console.log('I am submitteed');
+        console.log(this.state.rating);
+       // this.setState({rating: event.target.value})
     }
+    HandleChange = (event) => {
+        this.setState({rating: event.target.value})
+    }
+
     render() { 
         return (
-        <form method="post" onSubmit={this.ClickHandle}>
-        <select><option value='1'>One</option>
-        <option value='1'>two</option>
-        <option value='1'>three</option>
-        <option value='1'>four</option>
+        <form method="post"  onSubmit={this.ClickHandle}>
+        <select defaultValue = {this.state.rating} onChange={this.HandleChange}>
+        <option  value='1'>One</option>
+        <option value='2'>Two</option>
+        <option value='3'>Three</option>
+        <option value='4'>Four</option>
+        <option value='5'>Five</option>
         </select> 
         <button>Submit</button>
         </form>
